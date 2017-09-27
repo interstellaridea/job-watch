c854f18ec25440bd958763957fcb430148dfad96
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-09-27 01:32:12 -0700

    Edit Jobs#show page and format _form partial

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,11 @@
 module JobsHelper
+
+	def generate_radio_boxes
+		collection_radio_buttons :job, :level, Job.levels, :first, :first do |l|
+			l.radio_button +
+				l.label do
+					l.object.first.titleize
+				end
+		end
+	end
 end
