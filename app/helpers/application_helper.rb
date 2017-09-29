febd2f4d705285461ab82ea82e91be2d910d4d17
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-09-29 14:16:34 -0700

    Add default check to generatetor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 		def generate_radio_boxes model, attrib, collection
-		collection_radio_buttons model, attrib, collection, :first, :first do |l|
+		collection_radio_buttons model, attrib, collection, :first, :first, checked: 'non_specified' do |l|
 			l.radio_button +
 				l.label do
 					l.object.first.titleize
