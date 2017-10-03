cef4d4fde6f81e5e7a3839d31f560d2cc281e5e5
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-10-03 11:37:43 -0700

    Fix bug where cocoon event listeners were not working

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,27 +1,31 @@
 // Limits cocoon js to only one nested field
-$(function(){
- 	$("#items a.add_fields")
-      .on('cocoon:before-insert', function () {
-          check_to_hide_or_show_add_link();
-      })
-      .on('cocoon:after-insert', function () {
-          check_to_hide_or_show_add_link();
-      })
-      .on("cocoon:before-remove", function () {
-          check_to_hide_or_show_add_link();
-      })
-      .on("cocoon:after-remove", function () {
-          check_to_hide_or_show_add_link();
-      });
+  $(document).on('ready page:load turbolinks:load',function(){
+    $('#items a.add_fields').
+      data("association-insertion-method", 'append').
+      data("association-insertion-node", '#items');
+   	$("#items")
+        .on('cocoon:before-insert', function () {
+            check_to_hide_or_show_add_link();
+        })
+        .on('cocoon:after-insert', function () {
+            check_to_hide_or_show_add_link();
+        })
+        .on("cocoon:before-remove", function () {
+            check_to_hide_or_show_add_link();
+        })
+        .on("cocoon:after-remove", function () {
+            check_to_hide_or_show_add_link();
+        });
 
-  check_to_hide_or_show_add_link();
+    check_to_hide_or_show_add_link();
 
-	function check_to_hide_or_show_add_link() {
-		console.log($('.nested-fields').length);
-		if ($('.nested-fields').length == 1) {
-			$("a.add_fields").hide();
-		} else {
-			$("a.add_fields").show();
-		}
-	} 
-});
+  	function check_to_hide_or_show_add_link() {
+  		console.log($('.nested-fields').length);
+  		if ($('.nested-fields').length == 1) {
+  			$("a.add_fields").hide();
+  		} else {
+  			$("a.add_fields").show();
+  		}
+  	} 
+    
+  });
